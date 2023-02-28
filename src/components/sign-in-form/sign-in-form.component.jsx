c3775b2c6216af0694e315cc6edb805d0bd10688
DenySim <sim_deny81@yahoo.com>
2023-02-28 22:22:26 +1100

    fire

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component'
+import './sign-in-form.styles.scss';
+import Button from "../button/button.component";
+
+const defaultFormFields = {
+    email:'',
+    password:''
+}
+
+const SignInForm = () => {
+    const[formFields, setFormFields] = useState(defaultFormFields);
+    const{ email, password } = formFields;
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const signInWithGoogle = async () => {
+        const { user } = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try{
+            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            resetFormFields();
+            console.log(response);
+        }catch(e){
+
+        }
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormFields({...formFields, [name]:value});   //... => spread object
+    }
+    
+    return(
+        <div className='sign-up-container'>
+            <h2>Already have an account?</h2>
+            <h1>Sign in with your email and password</h1>
+            <form onSubmit={handleSubmit}>
+                <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email}/>
+                <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password}/>
+                <div className="buttons-container">
+                <Button buttonType='default' type="submit">Sign In</Button>
+                <Button buttonType='google' onClick={signInWithGoogle} type="submit">Google Sign In</Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SignInForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,7 +3,8 @@ import { getRedirectResult } from "firebase/auth";
 import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } 
 from "../../utils/firebase/firebase.utils";
 
-import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
 const SignIn = () => {
     useEffect(async()=>{
@@ -21,9 +22,7 @@ const SignIn = () => {
     return(
         <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google Popup
-            </button>
+            <SignInForm />
             <SignUpForm />
             <button onClick={signInWithGoogleRedirect} hidden={true}>
                 Sign in with Google Redirect
