875559572f0a265b6aed36bf9d2834ef152887b3
DenySim <sim_deny81@yahoo.com>
2023-02-28 21:45:40 +1100

    fire

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -3,10 +3,10 @@ import './form-input.styles.scss';
 const FormInput = ({label, ...otherProps}) =>{
     return(
         <div className="group">
+            <input className="form-input" {...otherProps} />
             { label && (
                 <label className={`${otherProps.value.length ? 'shrink': ''}form-input-label`}>{label}</label>
             )}         
-            <input className="form-input" {...otherProps} />
         </div>
     )
 }
