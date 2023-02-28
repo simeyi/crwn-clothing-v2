93222d9cfe6aa6f4c57247597f1cfb2f38c5005a
DenySim <sim_deny81@yahoo.com>
2023-02-28 20:21:15 +1100

    firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,19 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } 
+from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
+    useEffect(async()=>{
+        const response = await getRedirectResult(auth);
+        if(response){
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+    },[])
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-
     }
 
     return(
@@ -12,6 +21,9 @@ const SignIn = () => {
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
             </button>
         </div>
     )
