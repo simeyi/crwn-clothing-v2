97bac3aa8e2651dc4fbde3f56bdad6667140a053
DenySim <sim_deny81@yahoo.com>
2023-04-04 09:08:08 +1000

    commit

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,19 @@
-import './cart-dropdown.styles.scss';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../context/cart.context';
 import CartItem from '../cart-item/cart-item.component';
-import Button from '../button/button.component';;
+import Button from '../button/button.component';
+import './cart-dropdown.styles.scss';
+
+
 
 const CartDropdown = () => {
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    }
+
     const{cartItems} = useContext(CartContext);
     return(
         <div className='cart-dropdown-container'>
@@ -14,7 +23,7 @@ const CartDropdown = () => {
                 ))}
             
             </div>
-            <Button>Checkout</Button>
+            <Button onClick={goToCheckoutHandler}>Checkout</Button>
         </div>
     )
 }
