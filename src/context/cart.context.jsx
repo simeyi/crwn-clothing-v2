9fb8631072c477b8bf0f22810feffb545604b27f
DenySim <sim_deny81@yahoo.com>
2023-04-04 10:49:52 +1000

    commit

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -14,11 +14,27 @@ const addCartItem = (cartItems, productToAdd) =>{
 
 }
 
+const removeCartItem = (cartItems, productToRemove) =>{
+    const existingCartItem = cartItems.find(
+        (cartItem)=> cartItem.id === productToRemove.id 
+    );
+
+    if (existingCartItem.quantity === 1){
+        return cartItems.filter((cartItem)=>cartItem.id !== productToRemove.id);
+    }
+
+    return cartItems.map((cartItem)=>
+        cartItem.id === productToRemove.id ?
+        {...cartItem, quantity: cartItem.quantity - 1} : cartItem
+    )
+}
+
 export const CartContext = createContext({
     isCartOpen:false,
     setIsCartOpen: () => {},
     cartItems:[],
     addItemToCart:() => {},
+    removeItemToCart:() => {},
     cartCount:0
 })
 
@@ -37,6 +53,10 @@ export const CartProvider = ({children}) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
+    const removeItemToCart = (productToRemove)=>{
+        setCartItems(removeCartItem(cartItems, productToRemove));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, cartItems, cartCount};
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
